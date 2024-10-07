6c6730b5dc9b4d4bb979fe042c3a3d8a7ff90f76
Olivia Schwartz <ocmooney@gmail.com>
2024-10-07 14:44:14 -0400

    debugging server

--- arena-server/config/config.js
+++ arena-server/config/config.js
@@ -2,26 +2,26 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'oliviaschwartz',
-    password: process.env.DB_PASSWORD || 'welcomeArena123',
-    database: process.env.DB_NAME || 'arena_talent',
+    username: process.env.DB_USERNAME || 'your-dev-username',
+    password: process.env.DB_PASSWORD || 'your-dev-password',
+    database: process.env.DB_NAME || 'your-dev-database',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres'
   },
   test: {
-    username: process.env.DB_USERNAME || 'oliviaschwartz',
-    password: process.env.DB_PASSWORD || 'welcomeArena123',
-    database: process.env.DB_NAME || 'arena_talent_test',
+    username: process.env.DB_USERNAME || 'your-test-username',
+    password: process.env.DB_PASSWORD || 'your-test-password',
+    database: process.env.DB_NAME || 'your-test-database',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'DATABASE_URL', // Use Heroku's DATABASE_URL
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true, // Heroku requires SSL
-        rejectUnauthorized: false // Necessary for Heroku Postgres SSL
+        require: true,
+        rejectUnauthorized: false // This is needed for Heroku Postgres SSL connection
       }
     }
   }
