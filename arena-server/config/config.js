1b9db6ba30520844d348912d4ec6a6126c9849a7
Olivia Schwartz <ocmooney@gmail.com>
2024-10-07 14:15:03 -0400

    debugging

--- arena-server/config/config.js
+++ arena-server/config/config.js
@@ -16,10 +16,13 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USERNAME || 'oliviaschwartz',
-    password: process.env.DB_PASSWORD || 'welcomeArena123',
-    database: process.env.DB_NAME || 'arena_talent_production',
-    host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL', // Use Heroku's DATABASE_URL
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, // Heroku requires SSL
+        rejectUnauthorized: false // Necessary for Heroku Postgres SSL
+      }
+    }
   }
 }
