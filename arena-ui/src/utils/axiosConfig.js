d4c2e9b28ceb8dc521ddafd4216181eea5a3465a
Olivia Schwartz <ocmooney@gmail.com>
2024-10-07 16:27:55 -0400

    fixes

--- arena-ui/src/utils/axiosConfig.js
+++ arena-ui/src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5002', // Uses correct env variable for the backend
-  withCredentials: true // Allows credentials (like cookies) to be sent with requests
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5002',
+  withCredentials: true // This should allow cookies to be sent
 })
 
 export default instance
