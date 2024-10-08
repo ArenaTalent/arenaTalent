74dddd86af650004bfb072d5ef0abda3356aa631
Olivia Schwartz <ocmooney@gmail.com>
2024-10-08 08:47:03 -0400

    fixing axios config

--- arena-ui/src/utils/axiosConfig.js
+++ arena-ui/src/utils/axiosConfig.js
@@ -1,13 +1,25 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL:
-    'https://arena-talent-809eb598a3c0.herokuapp.com' ||
-    'http://localhost:5002',
+  baseURL: 'https://arena-talent-809eb598a3c0.herokuapp.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
   }
 })
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem('authToken') // Adjust this based on how you store your token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
 
 export default instance
